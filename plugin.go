2dd7598a28f36248e29dbdd32065ca7eb574caed
bing.he <bing.he@ihandysoft.com>
2020-04-24 13:12:24 +0800

    fix parameter error

--- plugin.go
+++ plugin.go
@@ -383,6 +383,7 @@ func (p *Plugin) Exec() error {
 	if serr != nil {
 		return serr
 	}
+	
 	fmt.Println(sresp)
 	fmt.Println("check task tag")
 	var taskTags []*ecs.Tag
@@ -398,10 +399,9 @@ func (p *Plugin) Exec() error {
 	}
 	fmt.Println("wait 5s")
 	time.Sleep(time.Duration(5) * time.Second)
-	fmt.Println("begin tot list tasks")
+	fmt.Println("begin tag list tasks")
 	listTaskInput := &ecs.ListTasksInput{
-		Cluster:     aws.String(p.Cluster),
-		Family:      aws.String(p.Family),
+		Cluster:     sresp.Service.ClusterArn,
 		ServiceName: sresp.Service.ServiceName,
 	}
 	listTaskOutput, err := svc.ListTasks(listTaskInput)
